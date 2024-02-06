e8038c419acf0d24ae8d969b9ba9e96c27021ca1
Manuel den Hartog <145329293+Manueldh@users.noreply.github.com>
2024-02-06 15:05:03 +0100

    First push

---
+++ index.js
@@ -0,0 +1,83 @@
+let player = {
+    name: "Manuel",
+    chips: 200
+}
+
+let cards = []
+let sum = 0
+let hasBlackJack = false
+let isAlive = false
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+let playerEl = document.getElementById("player-el")
+
+playerEl.textContent = player.name + ": $" + player.chips
+
+function getRandomCard() {
+    let randomNumber = Math.floor( Math.random()*13 ) + 1
+    if (randomNumber > 10) {
+        return 10
+    } else if (randomNumber === 1) {
+        return 11
+    } else {
+        return randomNumber
+    }
+}
+
+function startGame() {
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard]
+    sum = firstCard + secondCard
+    renderGame()
+}
+
+function renderGame() {
+    cardsEl.textContent = "Cards: "
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " "
+    }
+    
+    sumEl.textContent = "Sum: " + sum
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+        hasBlackJack = false
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+        hasBlackJack = false
+    }
+    messageEl.textContent = message
+    
+    function addChips() {
+        player.chips += 50;
+    }
+    function removeChips() {
+        player.chips -= 1;
+    }
+    
+    if(hasBlackJack === true){
+        addChips()
+        playerEl.textContent = player.name + ": $" + player.chips
+    }
+    if(isAlive === false){
+        removeChips()
+        playerEl.textContent = player.name + ": $" + player.chips
+    }
+}
+
+
+function newCard() {
+    if (isAlive === true && hasBlackJack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()        
+    }
+}
